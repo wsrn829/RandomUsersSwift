fc926770a7ecdd30dc2bdb0a468bfcb4f8bfc887
wsrn829 <wsrn829@gmail.com>
2024-05-26 19:26:35 -0400

    Complete UserFetchingClient.swift

---
+++ RandomUsers/UserData.swift
@@ -0,0 +1,12 @@
+//
+//  UserData.swift
+//  RandomUsers
+//
+//  Created by Sarina on 5/26/24.
+//
+
+import Foundation
+
+class UserData {
+    var users: String = ""
+}
---
+++ RandomUsers/UserFetchingClient.swift
@@ -0,0 +1,18 @@
+//
+//  UserFetchingClient.swift
+//  RandomUsers
+//
+//  Created by Sarina on 5/26/24.
+//
+
+import Foundation
+
+struct UserFetchingClient {
+    static private let url = URL(string: "https://randomuser.me/api/?results=10&format=pretty")!
+    
+    static func getUsers() async throws -> String {
+        async let (data, _) = URLSession.shared.data(from: url)
+        return try await String(data: data, encoding: .utf8)!
+    }
+}
+
