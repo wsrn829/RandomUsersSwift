5f018189e31c825b97f8e08fe2a42ee5ffeb4c1f
wsrn829 <wsrn829@gmail.com>
2024-05-26 13:40:34 -0400

    Initial Commit

---
+++ RandomUsers/RandomUsersApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomUsersApp.swift
+//  RandomUsers
+//
+//  Created by S on 5/26/24.
+//
+
+import SwiftUI
+
+@main
+struct RandomUsersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
